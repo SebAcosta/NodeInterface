f5e04f8e649a551630135608882d5fb5307b6279
Sebastian <eabastian@gmail.com>
2022-12-13 19:56:34 -0600

    Added files

---
+++ Administracion/js/login.js
@@ -0,0 +1,33 @@
+window.onload = init;
+const API_URL = 'http://localhost:3000';
+
+function init(){
+    if(!sessionStorage.getItem("token")){
+        document.getElementById('button').addEventListener('click',login)
+    }else{
+        window.location.href = "home.html"
+    }
+}
+
+function login(){
+    var mail = document.getElementById('input-mail').value
+    var pass = document.getElementById('input-password').value
+
+    axios({
+        method:'post',
+        url:API_URL+'/user/login',
+        data:{
+            correo: mail,
+            pass: pass
+        }
+    }).then(function(res){
+        if(res.data.code === 200){
+            sessionStorage.setItem("token",res.data.message)
+            window.location.href ="home.html"
+        }else{
+            alert("Usuario y/o contraseña incorrectos")
+        }
+    }).catch(function(err){
+        console.log(err)
+    })
+}
